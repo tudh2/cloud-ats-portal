9e0664b864e96a1911ec5ba65fc6d7ccdc95b810
haint <haithanh0809@gmail.com>
2015-05-18 15:15:10 +0700

    Select space

--- public/app/layout/directives/list-space.js
+++ public/app/layout/directives/list-space.js
@@ -8,11 +8,25 @@ define(['layout/module'], function(module) {
       templateUrl: 'app/layout/directives/list-space.tpl.html',
       link: function(scope, element) {
         
-        scope.space = 'Public';
+        if ($rootScope.context.space !== undefined) {
+          scope.space = $rootScope.context.space.name;
+        } else {
+          scope.space = 'Public';
+        }
 
         UserService.spaces().then(function(spaces) {
           scope.spaces = spaces;
         });
+
+        scope.select = function(space) {
+          if (space === undefined) {
+            scope.space = 'Public';
+            UserService.go({_id : null});
+          } else {
+            scope.space = space.name;
+            UserService.go(space);
+          }
+        }
       }
     }
   }]);
--- public/app/services/user-service.js
+++ public/app/services/user-service.js
@@ -1,7 +1,8 @@
 define(['app'], function(app) {
   'use strict';
 
-  app.factory('UserService', ['$http', '$q', '$cookies', function($http, $q, $cookies){
+  app.factory('UserService', ['$http', '$q', '$cookies', '$rootScope', '$window',
+    function($http, $q, $cookies, $rootScope, $window){
     return {
       spaces: function(){
         var dfd = $q.defer();
@@ -21,6 +22,21 @@ define(['app'], function(app) {
         .error(dfd.reject);
 
         return dfd.promise;
+      },
+
+      go: function(space) {
+        var request = {
+          method: 'GET',
+          url: 'http://localhost:9000/api/v1/user/go/' + space._id,
+          headers: {
+           'X-AUTH-TOKEN': $cookies.get('authToken') 
+          } 
+        }
+
+        $http(request).success(function(context) {
+          $window.sessionStorage.setItem('context', JSON.stringify(context));
+          $rootScope.context = context;
+        });
       }
     };
   }])
