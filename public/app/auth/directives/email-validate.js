e705f7dad18557c073b65ff88dc881845f8ea0fd
TrinhTV3 <trinhtv060392@gmail.com>
2015-05-20 17:12:20 +0700

    validate email with pattern

--- public/app/auth/directives/email-validate.js
+++ public/app/auth/directives/email-validate.js
@@ -6,8 +6,20 @@ define(['auth/module'], function(module) {
 		return {
 			restrict: 'A',
 			link: function(scope, element, attributes) {
+
+
 				element.on('blur', function() {
+					
 					var email = element.val();
+					var checkAccount = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(email);
+
+					if (checkAccount == false) {
+						scope.checkEmailPattern = true;
+						scope.notify = 'The email must contain "., @" character ';
+					} else {
+						scope.checkEmailPattern = false;
+						scope.notify = 'The email must contain "., @" character ';
+					}
 					AuthenticationService.checkAccount(email, function(data){
 						if (data == 'false') {
 	            scope.checkEmail = true;
