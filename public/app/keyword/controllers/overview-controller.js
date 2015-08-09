42b940e30f777c467d5862acdfaabcd12642a62b
haint <haithanh0809@gmail.com>
2015-08-09 15:51:05 +0700

    Keyword project view logs and watching test

--- public/app/keyword/controllers/overview-controller.js
+++ public/app/keyword/controllers/overview-controller.js
@@ -1,9 +1,9 @@
-define(['keyword/module'], function (module) {
+define(['keyword/module', 'lodash'], function (module, _) {
   'use strict';
 
   module.registerController('OverviewCtrl', 
-    ['$scope', '$rootScope', '$state', '$stateParams', '$cookies', 'KeywordService', 'EventService',
-    function($scope, $rootScope, $state, $stateParams, $cookies, KeywordService, EventService) {
+    ['$scope', '$rootScope', '$state', '$stateParams', '$templateRequest', '$compile', '$cookies', 'KeywordService', 'EventService',
+    function($scope, $rootScope, $state, $stateParams, $templateRequest, $compile, $cookies, KeywordService, EventService) {
 
       $scope.projectId = $stateParams.id;
 
@@ -13,11 +13,71 @@ define(['keyword/module'], function (module) {
         $scope.project = response;
       });
 
+      var loadModal = function() {
+        var $modal = $('#project-log');
+
+        //clear modal content
+        $modal.html('');
+
+        $templateRequest('app/keyword/views/overview-modal-content.tpl.html').then(function(template) {
+          $modal.html($compile(template)($scope));
+          $modal.modal('show');
+        });
+      }
+
+      $scope.viewLog = function() {
+        loadModal();
+      }
+
+      $scope.runLastSuites = function() {
+
+        $scope.project.status = "RUNNING";
+
+        var selected = [];
+        _.forEach($scope.project.lastSuites, function(sel) {
+          selected.push(sel._id);
+        });
+
+        KeywordService.run($scope.projectId, selected, function (data, status) {
+          switch (status) {
+            case 201:
+              $.smallBox({
+                title: 'Notification',
+                content: 'You have submitted project job',
+                color: '#296191',
+                iconSmall: 'fa fa-check bounce animated',
+                timeout: 3000
+              });
+              break;
+            case 204:
+              $.smallBox({
+                title: 'Notification',
+                content: 'Your project has been already running',
+                color: '#296191',
+                iconSmall: 'fa fa-check bounce animated',
+                timeout: 3000
+              });
+              break;
+            default:
+              $.smallBox({
+                title: 'Notification',
+                content: 'Can not submmit your project job',
+                color: '#c26565',
+                iconSmall: 'fa fa-ban bounce animated',
+                timeout: 3000
+              });
+
+          }
+        });
+      }
+
       var updateStatus = function(msg) {
         $scope.$apply(function() {
           var job = JSON.parse(msg.data);
           if (job.project_id === $scope.projectId) {
             $scope.project.status = job.project_status;
+            $scope.project.watchUrl = job.watch_url;
+            $scope.project.log = job.log;
           }
         })
       }
