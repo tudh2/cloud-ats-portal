c0423cae776bdab8500936ace5dba605ad6fb72e
haint <haithanh0809@gmail.com>
2015-08-11 10:00:31 +0700

    Project list filter & search

--- public/app/app.js
+++ public/app/app.js
@@ -90,5 +90,18 @@ define([
       $rootScope.$stateParams = $stateParams;
   }]);
 
+  app.filter('search', function ($filter) {
+    return function(items, text) {
+      if (!text || text.length === 0) return items;
+      var searchTerms = text.split(' ');
+
+      searchTerms.forEach(function(term) {
+        if (term && term.length) items = $filter('filter')(items, term);
+      });
+
+      return items;
+    }
+  })
+
   return app;
 });
