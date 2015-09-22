8161f0eea3a97d1600b90108b503005950daf482
TrinhTV3 <trinhtv060392@gmail.com>
2015-09-22 09:45:15 +0700

    split one request to two requests to reduce response data

--- public/app/performance/controllers/overview-controller.js
+++ public/app/performance/controllers/overview-controller.js
@@ -9,13 +9,16 @@ define(['performance/module', 'lodash', 'notification'], function (module, _) {
       $scope.title = 'OVERVIEWS'
       PerformanceService.get($scope.projectId, function(response) {
         $scope.project = response;
-        
-        if (response.jobs != null) {
-          var jobs = JSON.parse(response.jobs);
-          $scope.project.jobs = jobs;
+        if (response.lastScripts) {
+          getListJob($scope.project._id);
         }
       });
+      var getListJob = function(projectId) {
 
+        PerformanceService.getListJob(projectId,function(data,status) {
+         $scope.project.jobs = data;
+        });
+      }
       $scope.openJobReport = function (jobId) {
         $state.go('app.performance.report', {jobId : jobId});
       }
