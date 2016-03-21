c5650dd7764fe7eac37b7fc581e90ccc8c0520dd
tudh2 <minhtu.svptit@gmail.com>
2016-03-21 15:35:52 +0700

    present data for job report

--- public/app/keyword/controllers/suite-report-controller.js
+++ public/app/keyword/controllers/suite-report-controller.js
@@ -12,16 +12,32 @@ define(['keyword/module', 'lodash', 'c3'], function (module, _, c3) {
     var chart = c3.generate({
       bindto: '#chart1',
       data: {
+        x: 'Date',
         columns: [
-            ['Pass', 3, 20, 10, 40, 15, 25],
-            ['Fail', 13, 10, 24, 20, 15, 50]
+            ['Date', '2012-12-31', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05'],
+            ['Pass', 20, 10, 40, 15, 25],
+            ['Fail', 10, 24, 20, 15, 50],
+            ['Skip', 1, 4, 2, 5, 0]
         ],
         type: 'spline',
         colors: {
             Pass: 'rgb(21, 171, 159)',
             Fail: 'rgb(255, 79, 81)',
+            Skip: '#ffaa2a',
         },
+        },
+         axis: {
+        x: {
+            type: 'timeseries',
+            // if true, treat x value as localtime (Default)
+            // if false, convert to UTC internally
+            localtime: true,
+            tick: {
+                format: '%Y-%m-%d %H:%M:%S'
+            }
         }
+    }
+
        });
 
     var chart = c3.generate({
