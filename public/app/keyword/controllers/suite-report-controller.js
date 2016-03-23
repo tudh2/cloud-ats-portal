4c30a7d24f71062da436624bf63783941141a718
tudh2 <minhtu.svptit@gmail.com>
2016-03-23 10:02:58 +0700

    remove console and update present data

--- public/app/keyword/controllers/suite-report-controller.js
+++ public/app/keyword/controllers/suite-report-controller.js
@@ -7,7 +7,6 @@ define(['keyword/module', 'lodash', 'c3'], function (module, _, c3) {
     $scope.suiteId = $stateParams.suiteId;
     $scope.projectId = $stateParams.id;
     $scope.suiteReportId = $stateParams.suiteReportId;
-
     $scope.query = {
       'limit': 10,
       'current': 1,
@@ -20,17 +19,16 @@ define(['keyword/module', 'lodash', 'c3'], function (module, _, c3) {
       $scope.query.total = data.totalCase;
       $scope.suites = JSON.parse(data.suites);
       _.forEach($scope.case_reports, function (obj) {
-        obj.report = JSON.parse(obj.report);
-        console.log(obj.report);
+        if (obj.report) {
+          obj.report = JSON.parse(obj.report);
+        }
         if (obj.data_driven) {
           obj.data_source = JSON.parse(obj.data_source);
         }
       });
-      console.log($scope.case_reports);
     });
 
     $scope.redirectToTestCaseReport = function(id) {
-      console.log(id);
       $state.go('app.keyword.report.suite.testcase', {'caseReportId': id});
     }
 
